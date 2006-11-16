47a1b6442ead38669be3401b31f418916ff93f8f
koconnor <koconnor>
2006-11-16 02:21:15 +0000

    Some additional function definitions.

--- include/pkfuncs.h
+++ include/pkfuncs.h
@@ -6,7 +6,9 @@
 // http://msdn.microsoft.com/library/default.asp?url=/library/en-us/wcemain4/html/cmconKernelFunctions.asp
 //
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
 VOID ForcePageout(void);
 LPVOID AllocPhysMem(DWORD cbSize, DWORD fdwProtect, DWORD dwAlignmentMask,
@@ -16,7 +18,12 @@ DWORD SetProcPermissions(DWORD newperms );
 DWORD GetCurrentPermissions(void);
 BOOL SetKMode(BOOL fMode);
 BOOL VirtualCopy(LPVOID lpvDest, LPVOID lpvSrc, DWORD cbSize, DWORD fdwProtect);
+BOOL LockPages(LPVOID lpvAddress, DWORD cbSize, PDWORD pPFNs, int fOptions);
+BOOL UnlockPages(LPVOID lpvAddress, DWORD cbSize);
+#define LOCKFLAG_READ       0x004
 
+#ifdef __cplusplus
 } // extern "C"
+#endif
 
 #endif // pkfuncs.h
