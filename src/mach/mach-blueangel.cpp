599550ceb77139027b66b9e23d4c804a297dcfe6
koconnor <koconnor>
2006-12-02 16:44:40 +0000

    Hardcode BA memory to 128M.

--- src/mach/mach-blueangel.cpp
+++ src/mach/mach-blueangel.cpp
@@ -1,5 +1,6 @@
 #include "arch-pxa.h" // MachinePXA
 #include "mach-types.h"
+#include "memory.h" // memPhysSize
 //#include "asic3.h"
 
 class MachBlueangel : public MachinePXA {
@@ -9,13 +10,14 @@ public:
         OEMInfo[0] = L"PH20";
         machType = MACH_TYPE_BLUEANGEL;
     }
-#if 0
     void init() {
+#if 0
         asic3_gpio_base=0x0c000000;
         asic3_sdio_base=0x0e000000;
         asic3_bus_shift=1;
-    }
 #endif
+        memPhysSize=128*1024*1024;
+    }
 };
 
 REGMACHINE(MachBlueangel)
