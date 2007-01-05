3b98737ea453f0c60188d74656fe30fb6f519e17
pfalcon <pfalcon>
2007-01-05 09:13:51 +0000

    mach-h5000: Add machine def, based on haretlog.txt.

---
+++ src/mach/mach-h5000.cpp
@@ -0,0 +1,14 @@
+#include "arch-pxa.h" // MachinePXA
+#include "mach-types.h"
+//#include "asic3.h"
+
+class MachH5000 : public MachinePXA {
+public:
+    MachH5000() {
+        name = "H5000";
+        OEMInfo[0] = L"HP iPAQ h5";
+        machType = MACH_TYPE_H5400;
+    }
+};
+
+REGMACHINE(MachH5000)
