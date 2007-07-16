576910375b6b2468e4ce0cd95b3776c19202fc49
pfalcon <pfalcon>
2007-07-16 12:48:57 +0000

    Implement support for creating scalable (read: large-size) boot bundles. Such support is now integrated in standard HaRET version - during startup, it checks if it is actually a boot bundle, and if so, proceeds with executing it.

--- src/wince/haret.cpp
+++ src/wince/haret.cpp
@@ -1,6 +1,8 @@
 /*
     Poor Man's ARM Hardware Reverse Engineering Tool
     Copyright (C) 2003 Andrew Zabolotny
+    Copyright (C) 2006 Kevin O'Connor <kevin@koconnor.net>
+    Copyright (C) 2007 Paul Sokolovsky <pmiscml@gmail.com>
 
     For conditions of use see file COPYING
 */
@@ -13,6 +15,7 @@
  */
 
 #include <windows.h>
+#include "pkfuncs.h"
 #include <stdio.h> // _snwprintf
 #include <wctype.h> // iswspace
 
@@ -23,12 +26,27 @@
 #include "script.h" // scrExecute, setupCommands
 #include "machines.h" // setupMachineType
 #include "network.h" // scrListen
+#include "linboot.h"
 
 #define WINDOW_CLASS TEXT("pmret")
 #define WINDOW_TITLE TEXT("HaRET")
 
+static bool try_linboot(void);
+
 HINSTANCE hInst;
 HWND MainWindow = 0;
+struct linload_header {
+    char siganture[8];
+    int kernelSize;
+    int initrdSize;
+    int scriptSize;
+    int kernelMd5;
+    int initrdMd5;
+    int scriptMd5;
+};
+
+static FILE *exeFile;
+static struct linload_header header;
 
 static BOOL CALLBACK DialogFunc (HWND hWnd, UINT message, WPARAM wParam,
   LPARAM lParam)
@@ -94,6 +112,9 @@ WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
     // Setup haret.
     setupHaret();
 
+    if (try_linboot())
+	return 0;
+
     // Initialize sockets
     Output("Running WSAStartup");
     WSADATA wsadata;
@@ -110,4 +131,78 @@ WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,
     shutdownHaret();
 
     return 0;
+}
+
+
+//----------
+
+// Boot kernel linked into exe.
+static void
+ramboot(const char *cmd, const char *args)
+{
+    fseek(exeFile,
+	  -(header.kernelSize + header.initrdSize + header.scriptSize + sizeof(struct linload_header)), 
+	  SEEK_END);
+    
+    bootHandleLinux(exeFile, header.kernelSize, header.initrdSize);
+}
+REG_CMD(0, "RAMBOOT|LINUX", ramboot,
+        "RAMBOOTLINUX\n"
+        "  Start booting linux kernel. See HELP VARS for variables affecting boot.")
+
+static char script_data[4096];
+
+static void fatal(const char *msg)
+{
+    Output(msg);
+    shutdownHaret();
+    exit(1);
+}	
+
+static bool try_linboot(void)
+{
+    wchar_t exePathW[MAX_PATH];
+    char exePath[MAX_PATH];
+    
+    // Read trailer
+    GetModuleFileName(NULL, exePathW, MAX_PATH);
+    wcstombs(exePath, exePathW, sizeof(exePath));
+    exeFile = fopen(exePath, "rb");
+    if (!exeFile)
+	fatal(C_ERROR "Could not open executable");
+    fseek(exeFile, -sizeof(struct linload_header), SEEK_END);
+    int len = fread(&header, sizeof(struct linload_header), 1, exeFile);
+    if (len != 1)
+	fatal(C_ERROR "Could not read trailer");
+    if (strncmp(header.siganture, "HARET", 5))
+	//fatal(C_ERROR "Could not find trailer signature");
+	return false;
+	
+    STORE_INFORMATION storeInfo;
+    if (GetStoreInformation(&storeInfo) && storeInfo.dwFreeSize > 100 * 1024) {
+	/*DWORD ret = */SetSystemMemoryDivision((storeInfo.dwStoreSize - storeInfo.dwFreeSize) / 4096 + 50);
+	/*if (ret == SYSMEM_CHANGED)
+	    MessageBox(NULL, L"SYSMEM_CHANGED", L"SetSystemMemoryDivision", 1);
+	else if (ret == SYSMEM_MUSTREBOOT)
+	    MessageBox(NULL, L"SYSMEM_MUSTREBOOT", L"SetSystemMemoryDivision", 1);
+	else
+	    MessageBox(NULL, L"SYSMEM_FAILED", L"SetSystemMemoryDivision", 1);*/
+    }
+
+    fseek(exeFile,
+	  -(header.scriptSize + sizeof(struct linload_header)), 
+	  SEEK_END);
+	
+    int toRead = header.scriptSize < (int)sizeof(script_data) - 1 ? header.scriptSize : sizeof(script_data) - 1;
+    len = fread(script_data, 1, toRead, exeFile);
+    if (len != toRead)
+	fatal(C_ERROR "Could not read script");
+
+    // Run linked in script.
+    runMemScript(script_data);
+
+    // Shutdown.
+    shutdownHaret();
+
+    return true;
 }
