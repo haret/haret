6d8681c2c8ed6040c869d1a70f0a7a78abde8cb5
koconnor <koconnor>
2007-08-17 01:27:42 +0000

    Add support for watching GPIO irqs.

--- haretconsole/memalias.py
+++ haretconsole/memalias.py
@@ -117,12 +117,19 @@ Regs_omap850 = {
     0xfffecb00: ("IH1", (lambda bit: "IH1-%d" % bit)),
     0xfffe0000: ("IH2", (lambda bit: "IH2-%d" % bit)),
     0xfffe0100: ("IH2", (lambda bit: "IH2-%d" % (bit + 32))),
-    0xfffbc000: ("GPIO0", (lambda bit: "GPIO%d" % bit)),
-    0xfffbc800: ("GPIO1", (lambda bit: "GPIO%d" % (bit + 32))),
-    0xfffbd000: ("GPIO2", (lambda bit: "GPIO%d" % (bit + 64))),
-    0xfffbd800: ("GPIO3", (lambda bit: "GPIO%d" % (bit + 96))),
-    0xfffbe000: ("GPIO4", (lambda bit: "GPIO%d" % (bit + 128))),
-    0xfffbe800: ("GPIO5", (lambda bit: "GPIO%d" % (bit + 160))),
+    0xfffbc014: ("GIRQ1", (lambda bit: "GPIO%d" % bit)),
+    0xfffbc814: ("GIRQ2", (lambda bit: "GPIO%d" % (bit + 32))),
+    0xfffbd014: ("GIRQ3", (lambda bit: "GPIO%d" % (bit + 64))),
+    0xfffbd814: ("GIRQ4", (lambda bit: "GPIO%d" % (bit + 96))),
+    0xfffbe014: ("GIRQ5", (lambda bit: "GPIO%d" % (bit + 128))),
+    0xfffbe814: ("GIRQ6", (lambda bit: "GPIO%d" % (bit + 160))),
+
+    0xfffbc000: ("GPIO1", (lambda bit: "GPIO%d" % bit)),
+    0xfffbc800: ("GPIO2", (lambda bit: "GPIO%d" % (bit + 32))),
+    0xfffbd000: ("GPIO3", (lambda bit: "GPIO%d" % (bit + 64))),
+    0xfffbd800: ("GPIO4", (lambda bit: "GPIO%d" % (bit + 96))),
+    0xfffbe000: ("GPIO5", (lambda bit: "GPIO%d" % (bit + 128))),
+    0xfffbe800: ("GPIO6", (lambda bit: "GPIO%d" % (bit + 160))),
     }
 RegsList['ARCH:OMAP850'] = Regs_omap850
 
--- src/mach/arch-omap.cpp
+++ src/mach/arch-omap.cpp
@@ -19,9 +19,15 @@ MachineOMAP850::init()
 {
     runMemScript("set ramaddr 0x10000000\n"
                  // IRQs
-                 "addlist irqs P2V(0xfffecb00) 0x4000000 32 0\n"
-                 "addlist irqs P2V(0xfffe0000) 0 32 0\n"
+                 "addlist irqs P2V(0xfffecb00) 0x40601c2 32 0\n"
+                 "addlist irqs P2V(0xfffe0000) 0x0 32 0\n"
                  "addlist irqs P2V(0xfffe0100) 0 32 0\n"
+                 "addlist irqs P2V(0xfffbc014) 0 32 0\n"
+                 "addlist irqs P2V(0xfffbc814) 0 32 0\n"
+                 "addlist irqs P2V(0xfffbd014) 0 32 0\n"
+                 "addlist irqs P2V(0xfffbd814) 0 32 0\n"
+                 "addlist irqs P2V(0xfffbe014) 0 32 0\n"
+                 "addlist irqs P2V(0xfffbe814) 0 32 0\n"
                  // GPIOs
                  "addlist gpios P2V(0xfffbc000)\n"
                  "addlist gpios P2V(0xfffbc800)\n"
