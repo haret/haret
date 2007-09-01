8f04145999318e3fcafa8051a3dde14de5d2f1ec
koconnor <koconnor>
2007-09-01 14:32:32 +0000

    Add definitions for PXA clock registers.

--- haretconsole/regs_pxa.py
+++ haretconsole/regs_pxa.py
@@ -24,6 +24,14 @@ irqs1 = (
     (28, "TMR2"), (29, "TMR3"), (30, "RTC0"), (31, "RTC1"))
 irqs2 = ((0, "TPM"), (1, "QCap"))
 
+cken = (
+    (31, "AC97conf"), (25, "TPM"), (24, "QCap"), (23, "SSP1"), (22, "mem"),
+    (21, "MemoryStick"), (20, "imem"), (19, "Keypad"), (18, "USIM"),
+    (17, "MSL"), (16, "LCD"), (15, "pI2C"), (14, "I2C"), (13, "IR"),
+    (12, "MMC"), (11, "USBc"), (10, "USBh"), (9, "TMR"), (8, "I2S"),
+    (7, "BTUART"), (6, "FFUART"), (5, "STUART"), (4, "SSP3"),
+    (3, "SSP2"), (2, "AC97"), ("0,1", "PWM"))
+
 Regs_pxa27x = {
     0x40D00000: ("ICIP", irqs1), 0x40D0009C: ("ICIP2", irqs2),
     0x40E00048: ("GEDR0", regOneBits("GPIO")),
@@ -49,6 +57,19 @@ Regs_pxa27x = {
     0x40E00068: ("GAFR2_U", regTwoBits("AF", 80)),
     0x40E0006c: ("GAFR3_L", regTwoBits("AF", 96)),
     0x40E00070: ("GAFR3_U", regTwoBits("AF", 112)),
+
+    # Clocks
+    0x41300000: ("CCCR", (("0-4", "L"), ("7-10", "2N"), (25, "A"),
+                          (26, "PLL_EARLY_EN"), (27, "LCD_26"), (30, "PPDIS"),
+                          (31, "CPDIS"))),
+    0x41300004: ("CKEN", cken),
+    0x41300008: ("OSCC", (("5,6", "OSD"), (4, "CRI"), (3, "PIO_EN"),
+                          (2, "TOUT_EN"), (1, "OON"), (0, "OOK"))),
+    0x4130000c: ("CCSR", (("0-4", "L_S"), ("7-9", "2N_S"),
+                          (28, "PPLCK"), (29, "CPLCK"), (30, "PPDIS_S"),
+                          (31, "CPDIS_S"))),
+    "insn:ee160e10": ("CLKCFG", ((3, "B"), (2, "HT"), (1, "F"), (0, "T"))),
+    "insn:ee170e10": ("PWRMODE", ((3, "VC"), ("0-2", "M"))),
     }
 memalias.RegsList['ARCH:PXA27x'] = Regs_pxa27x
 
--- src/mach/arch-pxa27x.cpp
+++ src/mach/arch-pxa27x.cpp
@@ -26,33 +26,43 @@ MachinePXA27x::detect()
 void
 MachinePXA27x::init()
 {
-    runMemScript("set ramaddr 0xa0000000\n"
-                 // IRQs
-                 "addlist IRQS p2v(0x40D00000) 0x480 32 0\n"
-                 "addlist IRQS p2v(0x40D0009c) 0xfffffffc 32 0\n"
-                 "addlist IRQS p2v(0x40E00048) 0 32 0\n"
-                 "addlist IRQS p2v(0x40E0004c) 0 32 0\n"
-                 "addlist IRQS p2v(0x40E00050) 0 32 0\n"
-                 "addlist IRQS p2v(0x40E00148) 0 32 0\n"
-                 // GPIO levels
-                 "addlist GPIOS p2v(0x40E00000)\n"
-                 "addlist GPIOS p2v(0x40E00004)\n"
-                 "addlist GPIOS p2v(0x40E00008)\n"
-                 "addlist GPIOS p2v(0x40E00100)\n"
-                 // GPIO directions
-                 "addlist GPIOS p2v(0x40E0000C)\n"
-                 "addlist GPIOS p2v(0x40E00010)\n"
-                 "addlist GPIOS p2v(0x40E00014)\n"
-                 "addlist GPIOS p2v(0x40E0010C)\n"
-                 // GPIO alt functions
-                 "addlist GPIOS p2v(0x40E00054)\n"
-                 "addlist GPIOS p2v(0x40E00058)\n"
-                 "addlist GPIOS p2v(0x40E0005c)\n"
-                 "addlist GPIOS p2v(0x40E00060)\n"
-                 "addlist GPIOS p2v(0x40E00064)\n"
-                 "addlist GPIOS p2v(0x40E00068)\n"
-                 "addlist GPIOS p2v(0x40E0006c)\n"
-                 "addlist GPIOS p2v(0x40E00070)\n");
+    runMemScript(
+        "set ramaddr 0xa0000000\n"
+        // IRQs
+        "addlist IRQS p2v(0x40D00000) 0x480 32 0\n"
+        "addlist IRQS p2v(0x40D0009c) 0xfffffffc 32 0\n"
+        "addlist IRQS p2v(0x40E00048) 0 32 0\n"
+        "addlist IRQS p2v(0x40E0004c) 0 32 0\n"
+        "addlist IRQS p2v(0x40E00050) 0 32 0\n"
+        "addlist IRQS p2v(0x40E00148) 0 32 0\n"
+        // GPIO levels
+        "addlist GPIOS p2v(0x40E00000)\n"
+        "addlist GPIOS p2v(0x40E00004)\n"
+        "addlist GPIOS p2v(0x40E00008)\n"
+        "addlist GPIOS p2v(0x40E00100)\n"
+        // GPIO directions
+        "addlist GPIOS p2v(0x40E0000C)\n"
+        "addlist GPIOS p2v(0x40E00010)\n"
+        "addlist GPIOS p2v(0x40E00014)\n"
+        "addlist GPIOS p2v(0x40E0010C)\n"
+        // GPIO alt functions
+        "addlist GPIOS p2v(0x40E00054)\n"
+        "addlist GPIOS p2v(0x40E00058)\n"
+        "addlist GPIOS p2v(0x40E0005c)\n"
+        "addlist GPIOS p2v(0x40E00060)\n"
+        "addlist GPIOS p2v(0x40E00064)\n"
+        "addlist GPIOS p2v(0x40E00068)\n"
+        "addlist GPIOS p2v(0x40E0006c)\n"
+        "addlist GPIOS p2v(0x40E00070)\n"
+        // Clock & Power registers
+        "newvar CLOCKS GPIOS 'Architecture clock registers'\n"
+        "addlist CLOCKS p2v(0x41300000)\n" // CCCR
+        "addlist CLOCKS p2v(0x41300004)\n" // CKEN
+        "addlist CLOCKS p2v(0x41300008)\n" // OSCC
+        "addlist CLOCKS p2v(0x4130000C)\n" // CCSR
+        "addlist CLOCKS cp 14 0 6 0 0\n" // CLKCFG
+        "addlist CLOCKS cp 14 0 7 0 0\n" // PWRMODE
+        );
 }
 
 int
