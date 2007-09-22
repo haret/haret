e979588d35ba3f5a9bb17cb363a195e039143e1f
pfalcon <pfalcon>
2007-09-22 20:59:20 +0000

    make-bootbundle.py: In case output name is not specified, figure good name based on initrd (for Angstrom images).

--- tools/make-bootbundle.py
+++ tools/make-bootbundle.py
@@ -5,6 +5,7 @@ import os
 import stat
 import struct
 import getopt
+import re
 
 optlist, args = getopt.gnu_getopt(sys.argv[1:], "o:h?")
 opts = {}
@@ -15,8 +16,13 @@ if len(args) != 4:
     print "Usage: make-bootbundle.py [-o <outfile>] <path to haret.exe> <zImage> <initrd> <script>"
     sys.exit(0)
 
-outfile = opts.get("-o", "bootbundle.exe")
+outfile = opts.get("-o", None)
+if not outfile:
+    m = re.match(r"(.+?)-(.+?)-(.+?)-(glibc|uclibc)-(.+?)-(.+)-([0-9]{8})-(.+?)\.", args[2])
+    outfile = "%s-%s-liveramdisk-%s-%s-%s.exe" % (m.group(1), m.group(2), m.group(6), m.group(7), m.group(8))
 os.system("cat %s %s %s %s> %s"  % (args[0], args[1], args[2], args[3], outfile))
+
+#Angstrom-x11-image-glibc-ipk-2007.9-test-20070922-hx4700.rootfs
 
 exe = open(outfile, "r+b")
 kernelSt = os.stat(args[1])
