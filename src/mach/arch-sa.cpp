ba00fb1687b77df2fade1cda820a7c00cafd7795
koconnor <koconnor>
2008-07-29 23:54:21 +0000

    Patch to add GPIOs to StrongArm init - from "Pigeon".
        Just a minor update to arch-sa.cpp. Added archname, IRQs and GPIOs.
    
        Thanks.
    
    Pigeon.

--- src/mach/arch-sa.cpp
+++ src/mach/arch-sa.cpp
@@ -4,6 +4,7 @@
 MachineSA::MachineSA()
 {
     name = "Generic Intel StrongArm";
+    archname = "SA1110";
     CPUInfo[0] = L"SA1110";
     CPUInfo[1] = L"SA110";
 }
@@ -13,6 +14,34 @@ MachineSA::init()
 {
     runMemScript(
         "set ramaddr 0xc0000000\n"
+
+        // Interrupt controller irq pending (ICIP)
+        "addlist IRQS p2v(0x90050000) 0 32 0\n"
+
+        // GPIO edge detect status (GEDR)
+        "addlist IRQS p2v(0x90040018) 0 32 0\n"
+
+        // GPIO pin level (GPLR)
+        "addlist GPIOS p2v(0x90040000)\n"
+
+        // GPIO pin direction (GPDR)
+        "addlist GPIOS p2v(0x90040004)\n"
+
+        // GPIO output set (GPSR)
+        "addlist GPIOS p2v(0x90040008)\n"
+
+        // GPIO output clear (GPCR)
+        "addlist GPIOS p2v(0x9004000C)\n"
+
+        // GPIO rising edge (GRER)
+        "addlist GPIOS p2v(0x90040010)\n"
+
+        // GPIO falling edge (GFER)
+        "addlist GPIOS p2v(0x90040014)\n"
+
+        // GPIO alt function (GAFR)
+        "addlist GPIOS p2v(0x9004001C)\n"
+
         );
 }
 
