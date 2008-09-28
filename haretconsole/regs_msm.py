7c6604c838e5c7da58094160ab150cf7a4fda4bc
koconnor <koconnor>
2008-09-28 21:34:50 +0000

    Add IRQ names for regs_msm.py -- patch from "cr2".

--- haretconsole/regs_msm.py
+++ haretconsole/regs_msm.py
@@ -12,6 +12,23 @@ from memalias import regOneBits
 # MSM7xxx
 ######################################################################
 
+irqs0 = (
+    (0, "M2A_0"), (1, "M2A_1"),(2, "M2A_2"),(3, "M2A_3"),(4, "M2A_4"),(5, "M2A_5"),(6, "M2A_6"),
+    (7, "GP_TIMER"), (8,"DBG_TIMER"), (9, "UART1"), (10, "UART2"), (11, "UART3"),
+    (12, "UART1_RX"), (13, "UART2_RX"), (14, "UART3_RX"), (15, "USB_OTG"), (16, "MDDI_PRI"),
+    (17, "MDDI_EXT"), (18, "MDDI_CLIENT"), (19, "MDP"), (20, "GRAPHICS"), (21, "ADM_AARM"),
+    (22, "ADSP_A11"), (23, "ADSP_A11_A9"), 
+    (24, "SDC10"), (25, "SDC11"), (26, "SDC20"), (27, "SDC21"),
+    (28, "KEYB"), (29, "TOUCH_SSBI"), (30, "TOUCH1"),
+    (31, "TOUCH2"))
+
+irqs1 = (
+    (0, "GPIO1"), (1, "GPIO2"),(2, "PWB_I2C"),(3, "SOFTRST"),(4, "NAND_WR_ER"),(5, "NAND_OP"),(6, "PBUS_ARM11"),
+    (7, "AXI_MPU_SMI"), (8,"AXI_MPU_EBI1"), (9, "AD_HSSD"), (10, "A11_PMU"), (11, "A11_DMA"),
+    (12, "TSIF_IRQ"), (13, "UART1DM_IRQ"), (14, "UART1DM_RX"), (15, "USB_HS"), (16, "SDC30"),
+    (17, "SDC31"), (18, "SDC40"), (19, "SDC41"), (20, "UART2DM_RX"),
+    (21, "UART2DM_IRQ"))
+
 Regs_msm_gpio = {
     0xa9200800: ("out0", regOneBits("out0-")),
     0xa9200804: ("out2", regOneBits("out2-")),
@@ -34,18 +51,25 @@ Regs_msm_gpio = {
     0xa9300c08: ("out1_en", regOneBits("out1_en-")),
     0xa9300c20: ("in1", regOneBits("in1-")),
     0xa9300c60: ("intr1_en", regOneBits("intr1_en-")),
+
+#    0xa92008a0: ("irq0", regOneBits("GPIO", 0)),
+#    0xa9300c70: ("irq1", regOneBits("GPIO", 16)),
+#    0xa92008a4: ("irq2", regOneBits("GPIO", 43)),
+#    0xa92008a8: ("irq3", regOneBits("GPIO", 68)),
+#    0xa92008ac: ("irq4", regOneBits("GPIO", 95)),
     }
 
 Regs_msm7xxx = Regs_msm_gpio.copy()
 Regs_msm7xxx.update({
-        0xc0000000: ("IRQ0", regOneBits("IRQ")),
-        0xc0000004: ("IRQ1", regOneBits("IRQ", 32)),
+        0xc0000000: ("IRQ0", irqs0),
+        0xc0000004: ("IRQ1", irqs1),
     })
 memalias.RegsList['ARCH:MSM7xxx'] = Regs_msm7xxx
 
 Regs_msm7xxxA = Regs_msm_gpio.copy()
 Regs_msm7xxxA.update({
-        0xc0000080: ("IRQ0", regOneBits("IRQ")),
-        0xc0000084: ("IRQ1", regOneBits("IRQ", 32)),
+        0xc0000080: ("IRQ0", irqs0),
+        0xc0000084: ("IRQ1", irqs1),
+#        0xa92008b0: ("irq5", regOneBits("GPIO", 107)),
     })
 memalias.RegsList['ARCH:MSM7xxxA'] = Regs_msm7xxxA
